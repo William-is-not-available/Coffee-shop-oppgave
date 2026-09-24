fab80b9f8798070914229f945c58ed5e3faa2e13
William-is-not-available <09willot@danielsen-skoler.no>
2026-09-24 11:40:15 +0200

    la alle valgene in i funksjoner.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Coffee-shop-priskalkulator
-I dette repoet finner du utgangspunktet for å lage en priskalkulator til en kafé. Bytt ut README.md med en beskrivelse av prosjektet ditt.
+# Kode forklarelse
+Først printer den en meny så bruker kan se hvilke valg de har.
+så har jeg brukt if/elif/else for å velge mellom de forskjelige som setter forskjelig verdi på pris.
+if/elif/else er inni funksjoner for at koden ikke skal fortsete etter det er et invalid input.
+så til slutt printer den prisen på det brukeren bestilte.
+
+# Legge til mere
+For å legge til mere er det bare å skrive det inn på toppen så det står i menyen også legge det in som en nye elif med pris.
--- coffeeshop.py
+++ coffeeshop.py
@@ -7,47 +7,63 @@ print("+                               +")
 print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
 print("")
 print("We serve the following coffees:")
-print(" > Espresso")
-print(" > Americano")
-print(" > Latte")
-print(" > Cappuccino")
-print(" > Macchiato")
-print(" > Mocha")
-print(" > Flat White")
+print(" > Espresso      £2.50")
+print(" > Americano     £3")
+print(" > Latte         £2.50")
+print(" > Cappuccino    £3")
+print(" > Macchiato     £2.50")
+print(" > Mocha         £3.50")
+print(" > Flat White    £2.50")
 print("----------------------------")
 
 price = 0
-coffee = input("What type of coffee would you like?").title()
-if coffee=="Espresso":
-   price = price + 2.50
-elif coffee=="Americano":
-   price = price + 3
-elif coffee=="Latte":
-   price = price + 2.50
-elif coffee=="Cappuccino":
-   price = price + 3
-elif coffee=="Macchiato":
-   price = price + 2.50
-elif coffee=="Mocha":
-   price = price + 3.50
-elif coffee=="Flat White":
-   price = price + 2.50
+#lager en funksjon for at den ikke skal fortsete etter det er invalid option.
+def coffee_type():
+   coffee = input("What type of coffee would you like?").title()
+   if coffee=="Espresso":
+      price = price + 2.50
+   elif coffee=="Americano":
+      price = price + 3
+   elif coffee=="Latte":
+      price = price + 2.50
+   elif coffee=="Cappuccino":
+      price = price + 3
+   elif coffee=="Macchiato":
+      price = price + 2.50
+   elif coffee=="Mocha":
+      price = price + 3.50
+   elif coffee=="Flat White":
+      price = price + 2.50
+   else:
+      print("Invalid option. Please select a type of coffee from the menu.")
+      coffee_type()
+coffee_type()
 
 #velge hvor stor kafen skal være
-Size = input("What size of coffee would you like?")
-if Size=="M":
-   price = price + 0
-elif Size=="L":
-   price = price + 1
-elif Size=="XL":
-   price = price + 1.50
+def coffee_size():
+   Size = input("What size of coffee would you like?")
+   if Size=="M":
+      price = price + 0
+   elif Size=="L":
+      price = price + 1
+   elif Size=="XL":
+      price = price + 1.50
+   else:
+      print("Invalid option. Please select a type of coffee from the menu.")
+      coffee_size()
+   coffee_size()
 
 #hvor de vil være
-place = input("Do you want take away?")
-if place=="No":
-   price = price + 0
-elif place=="Yes":
-   price = price + 1
+def coffee_takeaway():
+   place = input("Do you want take away?")
+   if place=="No":
+      price = price + 0
+   elif place=="Yes":
+      price = price + 1
+   else:
+      print("Invalid option. Please select a type of coffee from the menu.")
+      coffee_takeaway()
+   coffee_takeaway()
 
 #Complete the code here...
 print("----------------------------")
